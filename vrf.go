9ca3f58655e6923a82919ade54b1438dd35c3355
Hyoung-yoon Kim <hykim.home@gmail.com>
2023-12-02 18:18:47 -0500

    add ProofToHash function

--- vrf.go
+++ vrf.go
@@ -5,6 +5,63 @@ import (
 	"fmt"
 )
 
+// Generates the VRF proof pi from a secret key and message.
+// Spec: `ECVRF_prove` function (section 5.1).
+func (v VRFStruct) Prove(secretKey, alpha []byte) ([]byte, error) {
+	// Step 1: derive public key from secret key as `Y = x * B`
+	publicKeyPoint := v.ScalarBaseMult(secretKey)
+	publicKeyBytes := v.MarshalCompressed(publicKeyPoint)
+
+	// Step 2: Encode to curve (using TAI)
+	hPoint, err := v.EncodeToCurveTAI(publicKeyBytes, alpha)
+	if err != nil {
+		return nil, err
+	}
+	hBytes := v.MarshalCompressed(hPoint)
+
+	// Step 4: Gamma = x * H
+	gammaPoint := v.ScalarMult(hPoint, secretKey)
+	gammaBytes := v.MarshalCompressed(gammaPoint)
+
+	// Step 5: nonce (k generation)
+	digest := v.Hash(hBytes)
+	kScalar := v.GenerateNonce(secretKey, digest)
+
+	// Step 6: c = ECVRF_challenge_generation (Y, H, Gamma, U, V)
+	// U = k*B
+	uPoint := v.ScalarBaseMult(kScalar)
+	uBytes := v.MarshalCompressed(uPoint)
+
+	// V = k*H
+	vPoint := v.ScalarMult(hPoint, kScalar)
+	vBytes := v.MarshalCompressed(vPoint)
+
+	// Challenge generation (returns hash output truncated by `cLen`)
+	var input []byte
+	input = append(input, publicKeyBytes...)
+	input = append(input, hBytes...)
+	input = append(input, gammaBytes...)
+	input = append(input, uBytes...)
+	input = append(input, vBytes...)
+
+	cScalar, err := v.ChallengeGeneration(input, v.CLen)
+	if err != nil {
+		return nil, err
+	}
+
+	// Step 7: s = (k + c*x) mod q
+	mul := v.ScalarMul(cScalar, secretKey)
+	sScalar := v.ScalarAdd(mul, kScalar)
+
+	// Step 8: encode (gamma, c, s)
+	var proof []byte
+	proof = append(proof, gammaBytes...)
+	proof = append(proof, cScalar...)
+	proof = append(proof, sScalar[:]...)
+
+	return proof, nil
+}
+
 // Verifies the provided VRF proof and computes the VRF hash output beta.
 // Spec: `ECVRF_verify` function (section 5.3).
 func (v VRFStruct) Verify(publicKey, pi, alpha []byte) ([]byte, error) {
@@ -73,59 +130,15 @@ func (v VRFStruct) Verify(publicKey, pi, alpha []byte) ([]byte, error) {
 	return v.gammaToHash(gammaPoint)
 }
 
-// Generates the VRF proof pi from a secret key and message.
-// Spec: `ECVRF_prove` function (section 5.1).
-func (v VRFStruct) Prove(secretKey, alpha []byte) ([]byte, error) {
-	// Step 1: derive public key from secret key as `Y = x * B`
-	publicKeyPoint := v.ScalarBaseMult(secretKey)
-	publicKeyBytes := v.MarshalCompressed(publicKeyPoint)
-
-	// Step 2: Encode to curve (using TAI)
-	hPoint, err := v.EncodeToCurveTAI(publicKeyBytes, alpha)
+// Spec: `ECVRF_proof_to_hash` function (section 5.2).
+func (v VRFStruct) ProofToHash(pi []byte) ([]byte, error) {
+	gamma, _, _, err := v.DecodeProof(pi)
 	if err != nil {
 		return nil, err
 	}
-	hBytes := v.MarshalCompressed(hPoint)
-
-	// Step 4: Gamma = x * H
-	gammaPoint := v.ScalarMult(hPoint, secretKey)
-	gammaBytes := v.MarshalCompressed(gammaPoint)
-
-	// Step 5: nonce (k generation)
-	digest := v.Hash(hBytes)
-	kScalar := v.GenerateNonce(secretKey, digest)
-
-	// Step 6: c = ECVRF_challenge_generation (Y, H, Gamma, U, V)
-	// U = k*B
-	uPoint := v.ScalarBaseMult(kScalar)
-	uBytes := v.MarshalCompressed(uPoint)
-
-	// V = k*H
-	vPoint := v.ScalarMult(hPoint, kScalar)
-	vBytes := v.MarshalCompressed(vPoint)
-
-	// Challenge generation (returns hash output truncated by `cLen`)
-	var input []byte
-	input = append(input, publicKeyBytes...)
-	input = append(input, hBytes...)
-	input = append(input, gammaBytes...)
-	input = append(input, uBytes...)
-	input = append(input, vBytes...)
-
-	cScalar, err := v.ChallengeGeneration(input, v.CLen)
+	gammaPoint, err := v.UnmarshalCompressed(gamma)
 	if err != nil {
 		return nil, err
 	}
-
-	// Step 7: s = (k + c*x) mod q
-	mul := v.ScalarMul(cScalar, secretKey)
-	sScalar := v.ScalarAdd(mul, kScalar)
-
-	// Step 8: encode (gamma, c, s)
-	var proof []byte
-	proof = append(proof, gammaBytes...)
-	proof = append(proof, cScalar...)
-	proof = append(proof, sScalar[:]...)
-
-	return proof, nil
+	return v.gammaToHash(gammaPoint)
 }
